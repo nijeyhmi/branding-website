a4602c096fd216939ce85a4b26bd72983e5c9b2c
nijeyhmi <nijeyhmi@gmail.com>
2025-05-17 20:44:56 +0900

    design: Header ui 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="fixed left-0 top-0 w-full bg-white">
+      <div className="flex items-center w-full h-[80px] justify-between px-32">
+        <h1 className="font-allura text-primary">AsYun</h1>
+        <ul className="flex flex-row text-primary text-2xl font-semibold">
+          <li>About Me</li>
+          <li className="pl-7">Videos</li>
+          <li className="pl-7">Shop</li>
+          <li className="pl-7">Guide</li>
+          <li className="pl-7">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
