fc0edeabc7c5f4429646d118c5044524df72fca2
nijeyhmi <nijeyhmi@gmail.com>
2025-05-18 14:11:42 +0900

    chore: nextjs 도입, vite 제거 및 프로젝트 세팅 수정

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+// src/pages/index.tsx
+import Header from "@/components/Header";
+import Main from "@/sections/Main";
+import About from "@/sections/About";
+import Contact from "@/sections/Contact";
+import Guide from "@/sections/Guide";
+import Shop from "@/sections/Shop";
+import Video from "@/sections/Video";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Header />
+      <main>
+        <Main />
+        <About />
+        <Video />
+        <Shop />
+        <Guide />
+        <Contact />
+      </main>
+    </div>
+  );
+}
