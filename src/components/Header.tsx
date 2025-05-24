d22a60cc87a371b1a0cf6e420b725b0339579f10
nijeyhmi <nijeyhmi@gmail.com>
2025-05-24 16:15:18 +0900

    design: Header ui 업데이트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <div className="fixed left-0 top-0 w-full bg-white">
-      <div className="flex items-center w-full h-[80px] justify-between px-32">
-        <h1 className="font-allura text-primary">AsYun</h1>
+      <div className="flex items-center w-full h-[120px] justify-between px-32">
+        <h1 className="font-allura text-primary !text-4xl">AsYun</h1>
         <ul className="flex flex-row text-primary text-2xl font-semibold">
           <li>About Me</li>
           <li className="pl-7">Videos</li>
